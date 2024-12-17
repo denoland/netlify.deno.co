3520df29a11c59fa28e284de0aea5b03d3e358b0
Bert Belder <bertbelder@gmail.com>
2024-12-17 09:14:55 -0800

    Log out `process` too

--- netlify/edge-functions/log.ts
+++ netlify/edge-functions/log.ts
@@ -2,11 +2,16 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log("Hello from the logging service. Here's window:");
-  console.log(window);
+  console.log(typeof window === "undefined" ? undefined : window);
+  console.log("Here's process:");
+  console.log(typeof process === "undefined" ? undefined : process);
 
-  return new Response("The request to this URL was logged. Bert modified it.", {
-    headers: { "content-type": "text/html" },
-  });
+  return new Response(
+    "The request to this URL was logged. Bert modified it again.",
+    {
+      headers: { "content-type": "text/html" },
+    },
+  );
 };
 
 export const config: Config = {
